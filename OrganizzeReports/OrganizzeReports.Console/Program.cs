46675c34cf3940e1a9c2a8b8ddf071d8b5cd0cd7
Caio Cruz <caio.cruz@gclaims.com.br>
2024-04-29 20:24:04 -0300

    Implements first feature

--- OrganizzeReports/OrganizzeReports.Console/Program.cs
+++ OrganizzeReports/OrganizzeReports.Console/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace OrganizzeReports.Console
+﻿using OrganizzeReports.Console.Adapters;
+using OrganizzeReports.Console.Services;
+
+namespace OrganizzeReports.Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Digite seu email:");
+            string email = System.Console.ReadLine();
+
+            System.Console.WriteLine("Digite seu nome:");
+            string name = System.Console.ReadLine();
+
+            System.Console.WriteLine("Digite sua apiKey:");
+            string apiKey = System.Console.ReadLine();           
+
+            var apiAdapter = new OrganizzeAPIAdapter(name, email, apiKey);
+            var reportService = new ReportService(apiAdapter);
+
+            Task.Run(async () =>
+            {
+                
+                var transactions = await reportService.GetTransactions();
+
+                
+                // Use os resultados aqui
+            }).Wait();
+
         }
     }
 }
