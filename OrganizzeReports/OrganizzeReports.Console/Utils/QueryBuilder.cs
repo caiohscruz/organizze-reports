aca9c2f765be7a87b46b889335faecdcfef1f4e8
Caio Cruz <caio.cruz@gclaims.com.br>
2024-04-30 20:47:50 -0300

    Add a classe to generate query strings

---
+++ OrganizzeReports/OrganizzeReports.Console/Utils/QueryBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizzeReports.Console.Utils
+{
+    public class QueryBuilder
+    {
+        private readonly List<string> _queryParameters = new List<string>();
+
+        public void Add(string key, string value)
+        {
+            _queryParameters.Add($"{Uri.EscapeDataString(key)}={Uri.EscapeDataString(value)}");
+        }
+
+        public override string ToString()
+        {
+            return "?" + string.Join("&", _queryParameters);
+        }
+    }
+}
