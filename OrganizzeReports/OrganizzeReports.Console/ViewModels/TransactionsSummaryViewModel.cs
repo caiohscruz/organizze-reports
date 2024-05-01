5608b443f621d348676e354763e5f51de3c2b33c
Caio Cruz <caio.cruz@gclaims.com.br>
2024-04-30 22:42:44 -0300

    Add Sumarry Report View Model

---
+++ OrganizzeReports/OrganizzeReports.Console/ViewModels/TransactionsSummaryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizzeReports.Console.ViewModels
+{
+    public class TransactionsSummaryViewModel
+    {
+        public string CategoryName { get; set; }
+        public decimal TotalCurrentMonth { get; set; }
+        public decimal TotalLastMonth { get; set; }
+        public decimal MonthlyProportionLast3Months { get; set; }
+        public decimal MonthlyProportionLast6Months { get; set; }
+        public decimal MonthlyProportionLast12Months { get; set; }
+        public decimal TotalLast3Months { get; set; }
+        public decimal TotalLast6Months { get; set; }
+        public decimal TotalLast12Months { get; set; }
+    }
+}
